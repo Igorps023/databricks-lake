eb5609469a10d228979011127fb7657e81bbcf38
Igorps023 <Igorps023>
2024-08-19 01:11:34 +0000

    ingestao full_load
    classe para leitura de catalogo, schema, tabela e formato

---
+++ src/bronze/Ingestion Lake.py
@@ -0,0 +1,44 @@
+# Databricks notebook source
+spark
+
+# COMMAND ----------
+
+print('Working')
+
+# COMMAND ----------
+
+catalog_name = "raw" 
+schema_name = "transactions"
+table_name = "transactions"
+format_type = "csv"
+
+# COMMAND ----------
+
+def load_data(catalog_name, schema_name, table_name, format_type):
+    """Loads data from source"""
+    dataframe = spark.read.format(f"{format_type}")\
+        .option("header", "true")\
+        .load(f"/Volumes/{catalog_name}/{schema_name}/{table_name}/")
+    
+    return df
+
+# COMMAND ----------
+
+teste = load_data("raw", "transactions", "transactions", "csv")
+
+# COMMAND ----------
+
+df = spark.read.format("csv")\
+    .option("header", "true")\
+    .load("/Volumes/raw/transactions/transactions/")
+df.show(4)
+
+# COMMAND ----------
+
+df.write.format("delta")\
+    .mode("overwrite")\
+    .saveAsTable("bronze.transactions.transactions")
+
+# COMMAND ----------
+
+
