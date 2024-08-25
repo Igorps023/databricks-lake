c7aacceaaa3ae4daee9f2295b33f179aab003c9c
Igorps023 <Igorps023>
2024-08-25 19:53:51 +0000

    Streaming (proc Batch com checkpoint)
    Utilizando streaming para processamento batch com o advento de checkpoint path

--- src/bronze/Ingestion Lake.py
+++ src/bronze/Ingestion Lake.py
@@ -3,6 +3,11 @@ spark
 
 # COMMAND ----------
 
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+import json
+
+# COMMAND ----------
+
 catalog_name = "raw" 
 schema_name = "transactions"
 table_name = "transactions"
@@ -10,20 +15,74 @@ format_type = "csv"
 
 # COMMAND ----------
 
-def load_data(catalog_name, schema_name, table_name, format_type):
+schema = StructType([
+    StructField("TransactionID", StringType(), True),
+    StructField("UserID", StringType(), True),
+    StructField("TransactionDate", StringType(), True),
+    StructField("TransactionValue", StringType(), True),
+    StructField("TransactionType", StringType(), True)
+])
+
+# COMMAND ----------
+
+def load_data(catalog_name, schema_name, table_name, format_type, schema):
     """Loads data from source"""
     dataframe = spark.read.format(f"{format_type}")\
         .option("header", "true")\
+        .schema(schema)\
         .load(f"/Volumes/{catalog_name}/{schema_name}/{table_name}/")
     
-    return df
+    return dataframe
 
 # COMMAND ----------
 
-ingestion_data = load_data("raw", "transactions", "transactions", "csv")
+# ingestion_data = load_data("raw", "transactions", "transactions", "csv", schema)
 
 # COMMAND ----------
 
-ingestion_data.write.format("delta")\
-    .mode("overwrite")\
-    .saveAsTable("bronze.transactions.transactions")
+# %sql
+# CREATE VOLUME IF NOT EXISTS "raw"."transactions"."transactions_checkpoint" 
+# LOCATION "s3:/Volumes/raw/transactions/transactions_checkpoint/"
+
+# %sql
+# CREATE VOLUME IF NOT EXISTS raw.transactions.transactions_checkpoint
+# LOCATION 's3://Volumes/raw/transactions/transactions_checkpoint/'
+
+# COMMAND ----------
+
+df_stream = (spark.readStream
+                  .format("cloudFiles")
+                  .option("cloudFiles.format", "csv")
+                  .option("header", "true")
+                  .schema(schema)
+                  .load(f"/Volumes/raw/transactions/transactions/")
+)
+
+def write_to_bronze_table(df, batchId):
+    df.write.format("delta")\
+        .mode("append")\
+        .saveAsTable("bronze.transactions.transactions")
+
+stream = (df_stream.writeStream
+                   .option("checkpointLocation", f"/Volumes/raw/transactions/transactions_checkpoint/")
+                   .foreachBatch(write_to_bronze_table)
+                   .trigger(availableNow=True)
+          )
+
+# COMMAND ----------
+
+start = stream.start()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- DELETE FROM bronze.transactions.transactions;
+# MAGIC -- SELECT * FROM bronze.transactions.transactions
+# MAGIC SELECT TransactionDate, count(*) FROM bronze.transactions.transactions GROUP BY TransactionDate ORDER BY TransactionDate DESC
+# MAGIC
+
+# COMMAND ----------
+
+# Criar uma funcao para ler tabelas que ja existem no catalogo
+# Se tabela nao existir, cria tabela
+# Se tabela existir, nao cria
